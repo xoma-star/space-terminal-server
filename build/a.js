24e6db9c9a2ce2906aa1d475949b65f19488405d
xoma_star <sasha.sirotyin09@gmail.com>
2024-02-28 16:36:30 +0500

    asd

--- build/a.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.biba = void 0;
-exports.biba = 1;
-//# sourceMappingURL=a.js.map
--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const a_1 = require("./a");
-const a = ['3'];
-console.log(a, a_1.biba);
-//# sourceMappingURL=index.js.map
